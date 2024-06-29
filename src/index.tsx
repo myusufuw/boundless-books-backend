2f0c596d88f3b9103d0683c1b3d29e824a10aa7e
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-06-29 14:36:56 +0700

    handle cors

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import { productsRoute } from "./products/routes"
 import { cors } from "hono/cors"
 import { authorRoute } from "./authors/routes"
 import { publisherRoute } from "./publishers/routes"
-import { bearerAuth } from "hono/bearer-auth"
 import { userRoute } from "./users/routes"
 
 const app = new OpenAPIHono()
+// HANDLE CORS
+app.use("/*", cors())
+
 // AUTH
 app.openAPIRegistry.registerComponent("securitySchemes", "Bearer", {
   type: "http",
@@ -16,51 +18,41 @@ app.openAPIRegistry.registerComponent("securitySchemes", "Bearer", {
 })
 
 // OPEN API
-app
-  .doc31("/docs", {
-    openapi: "3.0.0",
-    info: {
-      version: "1.0.0",
-      title: "Boundless Books REST API",
-      description:
-        "Boundless Books API is a backend application that provides various information about simple online book store.",
-    },
-  })
+app.doc31("/docs", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Boundless Books REST API",
+    description:
+      "Boundless Books API is a backend application that provides various information about simple online book store.",
+  },
+})
 
-  // AUTH
-  .use("/products", bearerAuth({ token: "asd" }))
+// ROUTES
+app.route("/products", productsRoute)
+app.route("/authors", authorRoute)
+app.route("/publishers", publisherRoute)
+app.route("/users", userRoute)
 
-  // HANDLE CORS
-  .use("/*", cors())
+// SWAGGER UI
+app.get("/ui", swaggerUI({ url: "/docs" }))
 
-  // ROUTES
-  .route("/products", productsRoute)
-  .route("/authors", authorRoute)
-  .route("/publishers", publisherRoute)
-  .route("/users", userRoute)
-
-  // SWAGGER UI
-  .get("/ui", swaggerUI({ url: "/docs" }))
-
-  // HOME PAGE
-  .get("/", (c) => {
-    return c.html(
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>Boundless Books API</title>
-          <meta name="description" content="Web API about books" />
-          <script src="https://cdn.tailwindcss.com"></script>
-        </head>
-        <body>
-          <HomePage />
-        </body>
-      </html>
-    )
-  })
+// HOME PAGE
+app.get("/", (c) => {
+  return c.html(
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Boundless Books API</title>
+        <meta name="description" content="Web API about books" />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>
+        <HomePage />
+      </body>
+    </html>
+  )
+})
 
 export default app
